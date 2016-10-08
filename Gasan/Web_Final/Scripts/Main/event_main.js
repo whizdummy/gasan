7a3f6b8b4de78b7df6030f4fcc6b3624a97b45a2
Jerald John Pormon <jeraldjohnpormon@yahoo.com>
2016-10-08 16:02:56 +0800

    Event render

--- Gasan/Web_Final/Scripts/Main/event_main.js
+++ Gasan/Web_Final/Scripts/Main/event_main.js
@@ -1,3 +1,21 @@
 ﻿app.controller('EventController', function ($timeout) {
+    var vm = this;
 
+    // Current and upcoming events
+    firebase.database().ref('events').orderByChild('isExpired').equalTo(false).on('value', function (data) {
+        $timeout(function () {
+            vm.currentEvents = data.val();
+        });
+    });
+
+    // Past events
+    firebase.database().ref('events').orderByChild('isExpired').equalTo(true).on('value', function (data) {
+        $timeout(function () {
+            vm.pastEvents = data.val();
+        });
+    });
+
+    vm.parseDate = function (date) {
+        return moment(date).format('MMMM DD, YYYY');
+    };
 });
