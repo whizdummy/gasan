ac0567d44289c3018ded0885a37e3c1ab6c00b0f
Jerald John Pormon <jeraldjohnpormon@yahoo.com>
2016-10-08 15:21:31 +0800

    Event update

--- Gasan/Web_Final/Scripts/Maintenance/event_maintenance.js
+++ Gasan/Web_Final/Scripts/Maintenance/event_maintenance.js
@@ -29,16 +29,38 @@
         vm.events = data.val();
     });
 
+    vm.editOnClick = function (id) {
+        vm.buttonType = 2;
+        vm.eventId = id;
+
+        firebase.database().ref('events/' + id).once('value', function (data) {
+            $timeout(function () {
+                var eventDetails = data.val();
+
+                vm.eventForm.title = eventDetails.title;
+                vm.eventForm.description = eventDetails.description;
+                vm.eventForm.startDate = new Date(eventDetails.startDate);
+                vm.eventForm.endDate = new Date(eventDetails.endDate);
+
+                eventDiv.slideDown();
+
+                vm.toggle = true;
+                vm.buttonName = "Close";
+            });
+        });
+    }
+
     vm.eventOnSubmit = function () {
         var file = document.getElementById('event-image').files[0];
+        var eventObject = {
+            title: vm.eventForm.title,
+            description: vm.eventForm.description,
+            startDate: (new Date(vm.eventForm.startDate)).getTime(),
+            endDate: (new Date(vm.eventForm.endDate)).getTime()
+        };
 
         if (vm.buttonType == 1) {
-            var eventDetails = firebase.database().ref('events').push({
-                title: vm.eventForm.title,
-                description: vm.eventForm.description,
-                startDate: (new Date(vm.eventForm.startDate)).getTime(),
-                endDate: (new Date(vm.eventForm.endDate)).getTime()
-            });
+            var eventDetails = firebase.database().ref('events').push(eventObject);
 
             if (eventDetails.key) {
                 if (file) {
@@ -79,6 +101,51 @@
             } else {
                 swal('Error', 'Something went wrong', 'error');
             }
+        } else if (vm.buttonType == 2) {
+            console.log(vm.eventId);
+
+            firebase.database().ref('events/' + vm.eventId).update(eventObject)
+                .then(function (data) {
+                    if (file) {
+                        firebase.storage().ref('images/events/' + vm.eventId + '.jpg').put(file)
+                            .then(function (data) {
+                                firebase.storage().ref('images/events/' + vm.eventId + '.jpg').getDownloadURL()
+                                .then(function (url) {
+                                    firebase.database().ref('events/' + vm.eventId).update({
+                                        imageUrl: url
+                                    }).then(function (data) {
+                                        $timeout(function () {
+                                            eventDiv.slideUp();
+
+                                            vm.toggle = false;
+                                            vm.buttonName = "Add";
+
+                                            vm.eventForm = {};
+                                        });
+
+                                        swal('Success', 'Event successfully added', 'success');
+                                    });
+                                }).catch(function (error) {
+                                    swal('Error', error.message, 'error');
+                                });
+                            }).catch(function (error) {
+                                swal('Error', error.message, 'error');
+                            })
+                    } else {
+                        $timeout(function () {
+                            eventDiv.slideUp();
+
+                            vm.toggle = false;
+                            vm.buttonName = "Add";
+
+                            vm.eventForm = {};
+                        });
+
+                        swal('Success', 'Event successfully updated', 'success');
+                    }
+                }).catch(function (error) {
+                    swal('Success', error.message, 'error');
+                });
         }
     }
 });
