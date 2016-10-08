0cd00ddac3db7248e96ed2adfe51f1c72615e7f0
Jerald John Pormon <jeraldjohnpormon@yahoo.com>
2016-10-09 01:28:34 +0800

    Navbar filter for admin

--- Gasan/Web_Final/Scripts/Main/admin_account.js
+++ Gasan/Web_Final/Scripts/Main/admin_account.js
@@ -1,14 +1,35 @@
-﻿function adminLoginOnSubmit() {
+﻿firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        console.log(firebase.auth().currentUser.uid);
+
+        $('li.user-auth-group').hide();
+    } else {
+        $('li.user-auth-group').show();
+        $('li.dropdown').hide();
+    }
+});
+
+function adminLoginOnSubmit() {
     var username = document.getElementById('username');
     var password = document.getElementById('password');
     var loginButton = document.getElementById('contact-submit');
 
     loginButton.disabled = true;
 
-    firebase.auth().signInWithEmailAndPassword(username.value.trim(), password.value.trim()).then(function (user) {
-        window.location.href = appSettings.basePath + "maintenance";
+    firebase.auth().signInWithEmailAndPassword(username.value.trim(), password.value.trim()).then(function () {
+        var user = firebase.auth().currentUser;
+
+        if (user) {
+            firebase.database().ref('users/' + user.uid).once('value', function (data) {
+                if (data.val().isAdmin) {
+                    $('li.user-auth-group').hide();
+
+                    window.location.href = appSettings.basePath + "maintenance";
+                }
+            });
+        }
     }).catch(function (error) {
-        alert(error.message);
+        swal('Error', error.message, 'error');
         loginButton.disabled = false;
     });
 }
