23dc8b8d1b06c5796dcc12460a43703afa3e8d89
Jerald John Pormon <jeraldjohnpormon@yahoo.com>
2016-10-08 20:51:34 +0800

    Official update and delete

--- Gasan/Web_Final/App_Start/BundleConfig.cs
+++ Gasan/Web_Final/App_Start/BundleConfig.cs
@@ -32,7 +32,8 @@ namespace Web_Final
                         "~/Scripts/Maintenance/announcement_maintenance.js",
                         "~/Scripts/Maintenance/event_maintenance.js",
                         "~/Scripts/Maintenance/banner_maintenance.js",
-                        "~/Scripts/Maintenance/position_maintenance.js"));
+                        "~/Scripts/Maintenance/position_maintenance.js",
+                        "~/Scripts/Maintenance/official_maintenance.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
---
+++ Gasan/Web_Final/Scripts/Maintenance/official_maintenance.js
@@ -0,0 +1,148 @@
+﻿app.controller('OfficialMaintenance', function ($timeout) {
+    var vm = this;
+    var officialList = [];
+
+    firebase.database().ref('positions').on('value', function (data) {
+        $timeout(function () {
+            vm.positions = data.val();
+        });
+    });
+
+    firebase.database().ref('officials').on('value', function (data) {
+        officialList = [];
+
+        data.forEach(function (childData) {
+            var detail = childData.val();
+
+            firebase.database().ref('positions/' + detail.position).once('value', function (snapshot) {
+                officialList.push({
+                    id: childData.key,
+                    firstName: detail.firstName,
+                    middleName: detail.middleName,
+                    lastName: detail.lastName,
+                    imageUrl: detail.imageUrl,
+                    description: detail.description,
+                    position: snapshot.val().name
+                });
+            });
+        });
+
+        $timeout(function () {
+            vm.officials = officialList;
+        });
+    });
+
+    vm.editOnClick = function (id) {
+        vm.buttonType = 2;
+        vm.officialId = id;
+
+        firebase.database().ref('officials/' + id).once('value', function (data) {
+            $timeout(function () {
+                var officialDetails = data.val();
+
+                vm.officialForm.firstName = officialDetails.firstName;
+                vm.officialForm.middleName = officialDetails.middleName;
+                vm.officialForm.lastName = officialDetails.lastName;
+                vm.officialForm.description = officialDetails.description;
+            });
+        });
+    }
+
+    vm.deleteOnClick = function (id) {
+        swal({
+            title: "Are you sure?",
+            text: null,
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Yes, delete it!",
+            cancelButtonText: "No, cancel it!",
+            closeOnConfirm: false,
+            closeOnCancel: false
+        }, function (isConfirm) {
+            if (isConfirm) {
+                firebase.database().ref('officials/' + id).remove()
+                    .then(function (data) {
+                        swal("Deleted!", "Official successfully deleted", "success");
+                    }).catch(function (error) {
+                        swal('Error', error.message, 'error');
+                    });
+            } else {
+                swal("Cancelled", null, "error");
+            }
+        });
+    }
+
+    vm.officialOnSubmit = function () {
+        var file = document.getElementById('official-image').files[0];
+        var officialObject = {
+            firstName: vm.officialForm.firstName,
+            middleName: vm.officialForm.middleName != null ? vm.officialForm.middleName : '',
+            lastName: vm.officialForm.lastName,
+            position: vm.officialForm.position,
+            description: vm.officialForm.description != null ? vm.officialForm.description : ''
+        };
+
+        if (vm.buttonType == 2) {
+            firebase.database().ref('officials/' + vm.officialId).update(officialObject)
+                .then(function (data) {
+                    if (file) {
+                        firebase.storage().ref('images/officials').child(vm.officialId + '.jpg').put(file)
+                            .then(function (data) {
+                                firebase.storage().ref('images/officials').child(vm.officialId + '.jpg').getDownloadURL()
+                                    .then(function (url) {
+                                        firebase.database().ref('officials/' + vm.officialId).update({
+                                            imageUrl: url
+                                        }).then(function (data) {
+                                            vm.officialForm = {};
+
+                                            swal('Success', 'Official successfully updated', 'success');
+                                        }).catch(function (error) {
+                                            swal('Error', error.message, 'error');
+                                        })
+                                    }).catch(function (error) {
+                                        swal('Error', error.message, 'error');
+                                    })
+                            }).catch(function (error) {
+                                swal('Error', error.message, 'error');
+                            });
+                    } else {
+                        vm.officialForm = {};
+
+                        swal('Success', 'Official successfully updated', 'success');
+                    }
+                });
+        } else {
+            var officialDetails = firebase.database().ref('officials').push(officialObject);
+
+            if (officialDetails.key) {
+                if (file) {
+                    firebase.storage().ref('images/officials').child(officialDetails.key + '.jpg').put(file)
+                        .then(function (data) {
+                            firebase.storage().ref('images/officials').child(officialDetails.key + '.jpg').getDownloadURL()
+                                .then(function (url) {
+                                    firebase.database().ref('officials/' + officialDetails.key).update({
+                                        imageUrl: url
+                                    }).then(function (data) {
+                                        $timeout(function () {
+                                            vm.officialForm = {};
+                                        });
+
+                                        swal('Success', 'Official successfully created', 'success');
+                                    }).catch(function (error) {
+                                        swal('Error', error.message, 'error');
+                                    })
+                                });
+                        }).catch(function (error) {
+                            swal('Error', error.message, 'error');
+                        });
+                } else {
+                    vm.officialForm = {};
+                    swal('Success', 'Official successfully created', 'success');
+                }
+            }
+        }
+
+        vm.buttonType = 1;
+    }
+});
