43f3ef7b729b0c5a70fe827672513ad328a12126
Jerald John Pormon <jeraldjohnpormon@yahoo.com>
2016-10-08 13:34:47 +0800

    Announcement render

--- Gasan/Web_Final/Scripts/Main/announcement_main.js
+++ Gasan/Web_Final/Scripts/Main/announcement_main.js
@@ -2,8 +2,23 @@
     var vm = this;
     var announcementRef = firebase.database().ref('announcements');
 
+    function checkExpiration(date, duration) {
+        var expiryDate = moment(date);
+        var currentDate = moment();
+        
+        return currentDate.isAfter(expiryDate.add(duration, 'h'));
+    }
+
     announcementRef.orderByChild('isExpired').equalTo(false).on('value', function (data) {
         $timeout(function () {
+            data.forEach(function (childData) {
+                if (checkExpiration(childData.val().updatedAt, childData.val().duration)) {
+                    firebase.database().ref('announcements/' + childData.key).update({
+                        isExpired: true
+                    });
+                }
+            });
+
             vm.currentAnnouncements = data.val();
         });
     });
@@ -15,8 +30,12 @@
     });
 
     vm.parseDate = function (unixTimestamp) {
-        var date = moment(unixTimestamp).format("MMMM DD, YYYY");
-        console.log(date);
+        var date = moment(unixTimestamp).format("MMMM DD, YYYY (hh:mm a)");
+
+        //console.log('dapat ', moment(unixTimestamp).add(2, 'h').format("MMMM DD, YYYY (hh:mm a)"));
+        //console.log('ngayon ', moment().format("MMMM DD, YYYY (hh:mm a)"));
+
+        //console.log(moment().isAfter(moment(unixTimestamp).add(2, 'h')));
 
         return date;
     }
